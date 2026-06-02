0cb1f417a03668cfa23340de8908f39d4d1ba813
Mila Balashova <mila.balashova2312@gmail.com>
2026-06-02 16:29:26 +0300

    update header scroll

--- source/scripts/modules/header.js
+++ source/scripts/modules/header.js
@@ -1,9 +1,9 @@
 const header = document.querySelector('.header');
-const nav = document.querySelector('.header__nav');
-const navBtn = document.querySelector('.header__nav-btn');
+// const nav = document.querySelector('.header__nav');
+// const navBtn = document.querySelector('.header__nav-btn');
 
 function setHeaderOnScroll() {
-  let prevScrollPosition = 0;
+  // let prevScrollPosition = 0;
   const SCROLL_THRESHOLD = 50;
 
   window.addEventListener('scroll', () => {
@@ -15,18 +15,18 @@ function setHeaderOnScroll() {
       header.classList.remove('header--default');
     }
 
-    if (currentScrollPosition > prevScrollPosition) {
-      header.classList.add('header--collapsed');
-      if (nav.classList.contains('is-active')) {
-        nav.classList.remove('is-active');
-      }
-      if (navBtn.classList.contains('is-active')) {
-        navBtn.classList.remove('is-active');
-      }
-    } else {
-      header.classList.remove('header--collapsed');
-    }
-    prevScrollPosition = Math.max(0, currentScrollPosition);
+    // if (currentScrollPosition > prevScrollPosition) {
+    //   header.classList.add('header--collapsed');
+    //   if (nav.classList.contains('is-active')) {
+    //     nav.classList.remove('is-active');
+    //   }
+    //   if (navBtn.classList.contains('is-active')) {
+    //     navBtn.classList.remove('is-active');
+    //   }
+    // } else {
+    //   header.classList.remove('header--collapsed');
+    // }
+    // prevScrollPosition = Math.max(0, currentScrollPosition);
   });
 }
 
