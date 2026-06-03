49f34e63c536691a0c8b2f9a36bbc5fd6dd4e587
Mila Balashova <mila.balashova2312@gmail.com>
2026-06-04 02:38:30 +0300

    client fixes

--- source/scripts/modules/header.js
+++ source/scripts/modules/header.js
@@ -1,9 +1,6 @@
 const header = document.querySelector('.header');
-// const nav = document.querySelector('.header__nav');
-// const navBtn = document.querySelector('.header__nav-btn');
 
 function setHeaderOnScroll() {
-  // let prevScrollPosition = 0;
   const SCROLL_THRESHOLD = 50;
 
   window.addEventListener('scroll', () => {
@@ -14,19 +11,6 @@ function setHeaderOnScroll() {
     } else {
       header.classList.remove('header--default');
     }
-
-    // if (currentScrollPosition > prevScrollPosition) {
-    //   header.classList.add('header--collapsed');
-    //   if (nav.classList.contains('is-active')) {
-    //     nav.classList.remove('is-active');
-    //   }
-    //   if (navBtn.classList.contains('is-active')) {
-    //     navBtn.classList.remove('is-active');
-    //   }
-    // } else {
-    //   header.classList.remove('header--collapsed');
-    // }
-    // prevScrollPosition = Math.max(0, currentScrollPosition);
   });
 }
 
--- source/scripts/modules/leaderboard.js
+++ source/scripts/modules/leaderboard.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-console */
-const API_URL = 'http://165.227.135.101:8000/leaderboard';
+const API_URL = 'http://165.227.135.101:8000/leaderboard?limit=10';
 
-async function loadLeaderboard(limit = 10) {
+async function loadLeaderboard() {
   try {
-    const response = await fetch(`${API_URL}}?limit=${limit}`);
+    const response = await fetch(API_URL);
     if (!response.ok) {
       console.log('API не ответил');
       throw new Error('API не ответил');
